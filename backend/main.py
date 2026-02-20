548910a12d811611e8d771592cc4be46df1d715e
Rakesh Kumar <rakeshkumar26978@gmail.com>
2026-02-20 12:13:36 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
